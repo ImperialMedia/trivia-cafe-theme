39daceb3857a72ad10a42b2b270bc183c52ecc3e
Bradmkjr <brad@ucanbonline.net>
2019-07-08 08:23:36 -0600

    working taxonomy

--- taxonomy-question_category.php
+++ taxonomy-question_category.php
@@ -15,6 +15,17 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
+<header class="page-header">
+	<div class="jumbotron">
+		<div class="container">
+		  <h1 class="display-4">
+		  	<?php echo single_term_title( '', false ); ?>
+		  </h1>
+		  <?php echo get_the_archive_description(); ?>
+		</div>
+	</div>
+</header><!-- .page-header -->
+
 <div class="wrapper" id="archive-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
@@ -24,19 +35,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
+
 			<main class="site-main" id="main">
 
 				<?php if ( have_posts() ) : ?>
 
-					<header class="page-header">
-						<h1><?php
-							// the_archive_title( '<h1 class="page-title">', '</h1>' );
-							echo single_term_title( '', false );
-						?></h1>
-						<?php
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
 
 					<!-- google text link ads -->
 					<script type="text/javascript"><!--
